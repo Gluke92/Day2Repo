937c095f5e3a58bdb9f4fc2078344bb876ab4679
zakdrummond <zakdrummond@gmail.com>
2018-01-16 13:27:54 -0800

    exercise 4

--- app.js
+++ app.js
@@ -120,19 +120,21 @@
 
 // [turtleStepsFwd, turtleStepslft]
 
-const testArray = [[0, 0], [0, 5], [-1, -3], [-3, 1], [2, -4], [3, 2]];
+let movements = [[0, 0], [0, 5], [-1, -3], [-3, 1], [2, -4], [3, 2]];
 
 // #1: Use the filter method to remove any items where the turtle moves backwards or to the right
 //remove negatives 
 // (i.e.where either the first of second number is an item is negative).
 
-console.log(testArray.filter(value => value[0] >= 0 && value[1] >= 0));
+// movements = testArray.filter(value => value[0] >= 0 && value[1] >= 0);
 
 // #2: Use the map method to create a new array containing how many steps the turtle makes in total with each movement
 //(i.e.the first and second number added together).
 
-console.log(testArray.map(value => (Math.abs(value[0]) + Math.abs(value[1]))));
+let distances = movements.map(value => (Math.abs(value[0]) + Math.abs(value[1])));
 
 // #3: Use the forEach method to log out how many steps the turtle took in each case.
 
-console.log(testArray.forEach(value => ))
+// console.log(testArray.forEach(steps, index) => testArray[index][0] + testArray[index][1]));
+
+distances.forEach((num, index) => console.log(`The turtle took ${num} steps.`));
